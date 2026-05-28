8e3c24cc32eb1d2da56ae4aaf86c19139ab20573
aboghith993-eng <alimubark440@gmail.com>
2026-05-28 06:27:54 +0300

    fix: remove --noinput from seed, add collectstatic

--- backend/start.sh
+++ backend/start.sh
@@ -1,3 +1,4 @@
 python manage.py migrate --noinput
-python manage.py seed --noinput
+python manage.py collectstatic --noinput
+python manage.py seed
 exec gunicorn ghithops.wsgi --bind 0.0.0.0:$PORT --workers 4 --timeout 120 --access-logfile -
